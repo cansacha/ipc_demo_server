e7f36bda7e542b73504d55962bd7fd80c4329f7f
Sacha Cantat <scantat01@gmail.com>
2025-06-21 17:11:27 +0200

    Object is named ipcDemoServer

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include "IpcDemoServer.hpp"
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main()
 {
-  std::cout << "Hello and welcome to DEvice manager" << std::endl;
+  std::cout << "Hello and welcome to ipc demo server" << std::endl;
 
-  IpcDemoServer deviceManager;
+  IpcDemoServer icpDemoServer;
 
   while (true) {
-    deviceManager.runLoop();
+    icpDemoServer.runLoop();
   }
   return 0;
 }
